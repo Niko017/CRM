ad5c1427d279134af60bc047373299e879d33212
Niko017 <nikolaydiaz.alu@iespacomolla.es>
2023-03-27 19:50:58 +0200

    fakl

---
+++ src/components/Editor.js
@@ -0,0 +1,140 @@
+import React, { useState} from 'react';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatColorFillIcon from '@mui/icons-material/FormatColorFill';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import StrikethroughSIcon from '@mui/icons-material/StrikethroughS';
+import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import TextField from '@mui/material/TextField';
+import Fuentes from 'components/Fuentes';
+
+function Editor (){
+
+    const [formato, setFormato] = useState([]);
+    const [alineo, setAlineo] = useState("");
+    const [font, setFont] = useState("");
+    const [size, setSize] = useState(8);
+
+    const handleFormat = (event, nuevoFormato) => {
+        setFormato(nuevoFormato);
+      };
+
+    const handleAlign = (event, nuevaAlineacion)=>{
+        setAlineo(nuevaAlineacion);
+    }
+
+    const handleFont = (event)=>{
+        setFont(event.target.value);
+    }
+
+    const handleSize = (event)=>{
+        setSize(event.target.value);
+            
+    }
+
+    return(
+        <Grid container spacing={1} columns={20} margin="normal">
+            <Paper elevation={1}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+        }}>
+                <ToggleButtonGroup aria-label="text formatting" value={formato} onChange={handleFormat}>
+                    <ToggleButton value="bold" aria-label="bold">
+                        <FormatBoldIcon />
+                    </ToggleButton>
+                    <ToggleButton value="italic" aria-label="italic">
+                        <FormatItalicIcon />
+                    </ToggleButton>
+                    <ToggleButton value="underlined" aria-label="underlined">
+                        <FormatUnderlinedIcon />
+                    </ToggleButton>
+                    <ToggleButton value="strike" aria-label="strike">
+                        <StrikethroughSIcon/>
+                    </ToggleButton>
+                    </ToggleButtonGroup>
+                    <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+                <ToggleButtonGroup exclusive value={alineo} onChange={handleAlign}>
+                    <ToggleButton value="left" aria-label="left aligned">
+                        <FormatAlignLeftIcon />
+                    </ToggleButton>
+                    <ToggleButton value="center" aria-label="centered">
+                        <FormatAlignCenterIcon />
+                    </ToggleButton>
+                    <ToggleButton value="right" aria-label="right aligned">
+                        <FormatAlignRightIcon />
+                    </ToggleButton>
+                    <ToggleButton value="justify" aria-label="justified">
+                        <FormatAlignJustifyIcon />
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+                <Box sx={{ minWidth: 110 }}>
+                    <FormControl fullWidth>
+                        <FormGroup fullWidth>
+                            <InputLabel id="fuenteID">Tipo</InputLabel>
+                            <Select
+                            labelId="fuenteID"
+                            id="demo-simple-select"
+                            value={font}
+                            label="Fuente"
+                            onChange={handleFont}
+                            defaultValue={'normal'}
+                            >
+                                <MenuItem value={10}>Normal</MenuItem>
+                                <MenuItem value={20}>H1</MenuItem>
+                                <MenuItem value={30}>H2</MenuItem>
+                                <MenuItem value={30}>H3</MenuItem>
+                                <MenuItem value={30}>H4</MenuItem>
+                                <MenuItem value={30}>Blockquote</MenuItem>
+                            </Select>
+                        </FormGroup>
+                    </FormControl>
+                </Box>
+                <Box sx={{ width: 100 }}>
+                    <TextField
+                            fullWidth
+                            type="number"
+                            label="Tamaño"
+                            value={size}
+                            onChange={handleSize}
+                    />
+                </Box>
+                <Box sx={{ minWidth: 110 }}>
+                    <FormControl fullWidth>
+                        <FormGroup fullWidth>
+                            <InputLabel id="fuenteID">Fuente</InputLabel>
+                            <Select
+                            labelId="fuenteID"
+                            id="demo-simple-select"
+                            value={font}
+                            label="Fuente"
+                            onChange={handleFont}
+                            >
+                                <MenuItem value={10}>Arail</MenuItem>
+                                <MenuItem value={20}>Times</MenuItem>
+                                <MenuItem value={30}>Robot</MenuItem>
+                            </Select>
+                        </FormGroup>
+                    </FormControl>
+                </Box>
+            </Paper>
+        </Grid>
+    );
+}
+export default Editor;
---
+++ src/components/Fuentes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import * as Font from './../fonts';
+
+function Fuentes(){
+
+    
+
+    return(
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+export default Fuentes;
