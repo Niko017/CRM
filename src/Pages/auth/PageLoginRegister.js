a7c84e1e667d2018a5fde035a1ece12530988a20
Niko017 <nikolaydiaz.alu@iespacomolla.es>
2023-04-25 20:08:56 +0200

    Login styles

--- src/Pages/auth/PageLoginRegister.js
+++ src/Pages/auth/PageLoginRegister.js
@@ -17,14 +17,21 @@ function PageLoginRegister(){
     contenedor.current.classList.remove("right-panel-active");
   }
 
+  const handleSubmitRegister = (event)=>{
+    event.preventDefault()
+    const datos = Object.fromEntries(new FormData(event.target))
+    console.log(datos);
+  }
+
   return(
     <React.Fragment>
   <h2>Inicio de Sesion</h2>
   <div className="container" ref={contenedor}>
+    {/* ////////CONTENEDOR DE REGISTRO////////////// */}
     <div className="form-container sign-up-container">
-      <form action="#">
-        <h1>Create Account</h1>
-        <div className="social-container">
+      <form onSubmit={handleSubmitRegister}>
+        <h1>Crear Cuenta</h1>
+        <div style={{margin:'10px 0px'}} className="social-container">
           <a href="/" className="social">
             <FacebookIcon htmlColor='#3b5998'/>
           </a>
@@ -36,16 +43,19 @@ function PageLoginRegister(){
           </a>
         </div>
         <span>or use your email for registration</span>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Sign Up</button>
+        <input type="text" name='user' placeholder="Usuario" />
+        <input type="email" name='email' placeholder="Email" /> 
+        <input type="password" name='pass' placeholder="Password" />
+        <input type="password" name='passConfirm' onPaste={(event)=>{event.preventDefault()}} placeholder='Confirmar Contraseña'/>
+        <button type='submit' style={{margin:'10px 0px'}}>Registrarse</button>
         <Link to='/'>Saltarse Inicio Sesion</Link>
       </form>
     </div>
+    {/* ///////////////FIN CONTENEDOR REGISTRO ///////////// */}
+    {/* ///////////////CONTENEDOR DE INICIO DE SESION/////// */}
     <div className="form-container sign-in-container">
       <form action="#">
-        <h1>Sign in</h1>
+        <h1>Inicio Sesión</h1>
         <div className="social-container">
           <a href="/" className="social">
             <FacebookIcon htmlColor='#3b5998'/>
@@ -57,25 +67,26 @@ function PageLoginRegister(){
             <LinkedInIcon htmlColor='#0e76a8'/>
           </a>
         </div>
-        <span>or use your account</span>
+        <span>o usa tu cuenta</span>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <a href="/">Forgot your password?</a>
+        <Link to="/">¿Has olvidado la contraseña?</Link>
         <button>Sign In</button>
       </form>
     </div>
+    {/* ////////////FIN CONTENEDOR INICIO DE SESION///////// */}
     <div className="overlay-container">
       <div className="overlay">
         <div className="overlay-panel overlay-left">
-          <h1>Welcome Back!</h1>
-          <p>To keep connected with us please login with your personal info</p>
+          <h1>Bienvenid@ de Nuevo!</h1>
+          <p>Inicia sesion para volver a la plataforma</p>
           <button className="ghost" onClick={handleInicioSesion}>
-            Sign In
+            Iniciar Sesion
           </button>
         </div>
         <div className="overlay-panel overlay-right">
-          <h1>Hello, Friend!</h1>
-          <p>Enter your personal details and start journey with us</p>
+          <h1>Hola, Bienvenid@</h1>
+          <p>Registrate para insgresar en nuestra plataforma </p>
           <button className="ghost" onClick={handleRegister}>
             Sign Up
           </button>
