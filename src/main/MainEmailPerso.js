d960460bb8a6f8fe37bd62a7cbb24971fbc7ce9c
Niko017 <nikolaydiaz.alu@iespacomolla.es>
2023-06-27 14:26:25 +0200

    fdfa

--- src/main/MainEmailPerso.js
+++ src/main/MainEmailPerso.js
@@ -10,6 +10,8 @@ import TablePerso from 'secciones/emailPersonales/TablePerso';
 import dayjs from 'dayjs';
 import axios from 'axios';
 import { BASE_URL } from 'constant/constantes';
+import { APIKEY } from 'constant/constantes';
+import { SENDING_EMAIL } from 'constant/constantes';
 
 function MainEmailPerso() {
 
@@ -43,6 +45,51 @@ function MainEmailPerso() {
     const handleEdit = () => {
         cargarEmailsPersonales();
         setEdit(prev => !prev);
+    }
+
+
+    const envioEmail = async (nombre, email) => {
+
+        const { correo, nombreEnvio } = JSON.parse(sessionStorage.getItem('user'));
+
+        const datos = {
+            sender: { name: nombreEnvio, email: correo },
+            to: [
+                {
+                    email,
+                    name: nombre,
+                }
+            ],
+            textContent: `<p>Estimados ${nombre},&nbsp;</p><p>Recordamos que el día 25 vence el plazo para la declaración de ventas.</p><p>Les solicitamos, por favor, sean puntuales para las mismas, ya que la no declaración es motivo suficiente para el rehúse de los avisos de insolvencia.</p><p>Por favor tened en cuenta que las ventas a crédito se declaran en el apartado "asegurables" y que las "no asegurables" deben declararse en su apartado correspondiente según motivo.</p><p>Gracias y un cordial saludo,</p><p>&nbsp;</p><p><span style="color: rgb(230, 0, 0);">***Horario del 15/06 al 15/09: Lunes a Jueves de 08:00 a 15:00 y Viernes de 8:00 a 14:00***</span></p>`,
+            subject: 'Declaracion de ventas',
+        }
+        const cabeceras = {
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                'api-key': `${APIKEY}`
+            }
+        }
+        await axios.post(SENDING_EMAIL, datos, cabeceras);
+
+
+    }
+
+    const enviarEmail = () => {
+        let emails = clientes.filter(cliente => {
+            if (cliente.email !== null && cliente.email !== " " && cliente.email !== "")
+                return cliente
+        });
+        let indice = 0;
+        console.log(emails);
+        let interval = setInterval(() => {
+            if (indice < emails.length) {
+                envioEmail(emails[indice].asegurado, emails[indice].email);
+                indice += 1;
+            } else {
+                clearInterval(interval);
+            }
+        }, 1500)
     }
 
     useEffect(() => {
@@ -85,7 +132,10 @@ function MainEmailPerso() {
                         >
                             <EditIcon />
                         </ToggleButton>
-                        <Button color='error' endIcon={<SendIcon />}>Enviar</Button>
+                        {
+                            filtrado.realizada ||
+                            <Button onClick={enviarEmail} color='error' endIcon={<SendIcon />}>Enviar</Button>
+                        }
                     </DemoContainer>
                 </LocalizationProvider>
             </Paper>
