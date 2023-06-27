08ea7b430d053ab94ef59b9de75a19d639d83024
Niko017 <113714855+Niko017@users.noreply.github.com>
2023-06-27 18:59:40 +0200

    Update constantes.js

--- src/constant/constantes.js
+++ src/constant/constantes.js
@@ -1,3 +1,3 @@
 export const SENDING_EMAIL  = 'https://api.sendinblue.com/v3/smtp/email';
 export const APIKEY = process.env.REACT_APP_API_KEY;
-export const BASE_URL = 'https://master--celebrated-zabaione-b9bfd6.netlify.app/';
+export const BASE_URL = 'https://master--celebrated-zabaione-b9bfd6.netlify.app';
